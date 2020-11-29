12ff6f806c0b7fcc8a606856fc8d16b6fa8cfb1c
owen <owen@owendeMacBook-Pro.local>
2020-11-29 11:07:16 -0500

    added more functions

--- src/main.py
+++ src/main.py
@@ -5,23 +5,22 @@ from skimage import (
 )
 
 from utils import (
-    loadImg,loadMat
+    loadMat,reconImg
 )
 
 if __name__ == "__main__":
 
     # light field image captured by Lytro ILLUM
     # demosaiced light field image, processed by MATLAB file
-    imgPath = "../data/lightfield.png"
-    # imgPath = "../data/stack.lfp"
     matPath = "../data/lightfield.mat"
 
-    # convert to 4D focal stack
-
-    # subImg = loadImg(imgPath)
-    # io.imshow(subImg)
-    # plt.show()
-
-    L= loadMat(matPath)      # L(u,v,s,t,c)
+    # read 5D focal stack, processed by MATLAB
+    L = loadMat(matPath)      # L(u,v,s,t,c)
+    [U,V,S,T,C] = L.shape
     print(L.shape)
+
+    imgRecon = reconImg(L)
+    io.imshow(imgRecon)
+    plt.show()
+
 
--- src/utils.py
+++ src/utils.py
@@ -5,36 +5,6 @@ from skimage import (
 )
 import scipy.io as sio
 
-
-def loadImg(imgPath):
-    # read lytro raw image, return L(u,v,s,t,c)
-    img = io.imread(imgPath)
-    [H,W,C] = img.shape
-    print(img.shape)
-    # lenslet span: 12*12
-    lenSize = 12
-    S = int(np.floor(H / lenSize))
-    T = int(np.floor(W / lenSize))
-
-    # img = img[3:,:,:]
-    # subImg = img[0:lenSize*1,
-    #          0:lenSize*4+int(lenSize/2),
-    #          :]
-    subImg = img[0:lenSize * 1,
-             lenSize * (T-4):W,
-             :]
-
-    # # rearrange to L(u,v,s,t,c), each lenslet is 16*16
-    # # L: 16*16*400*700*3
-    # L = np.zeros((16, 16, S, T, C))
-    # for s in range(0,S):
-    #     for t in range(0,T):
-    #         L[:,:,s,t,:] = img[s*16:(s+1)*16,t*16:(t+1)*16,:]
-    # print("generated L")
-    # return L      # L(u,v,s,t)
-
-    return subImg
-
 def loadMat(matPath):
     # read MATLAB processed 5D lightfield
     # L(u,v,s,t,c)
@@ -43,3 +13,15 @@ def loadMat(matPath):
     L = mat['LF']
 
     return L
+
+def reconImg(L):
+    # reconstruct light field image from 5D focal stack
+
+    [U, V, S, T, C] = L.shape
+    imgRecon = np.zeros((U*S,V*T,C))
+
+    for s in range(0,S):
+        for t in range(0,T):
+            imgRecon[s*U:(s+1)*U,t*V:(t+1)*V,:] = L[:,:,s,t,:]
+
+    return imgRecon
