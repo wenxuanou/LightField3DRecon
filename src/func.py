6d932f332d1bd581ab2f3d3526cea045c46d1de8
owen <owen@owendeMacBook-Pro.local>
2020-12-14 16:06:23 -0500

    fixed bugs, depth estimate finished

--- src/func.py
+++ src/func.py
@@ -20,49 +20,112 @@ def edgeConfidence(EPI,edgeThresh):
     Me = Ce > edgeThresh  # H*W
     return Ce,Me
 
-def getR_Horizontal(s,d,uHat,EPI_h):
+def getR_Horizontal(t,d,vHat,EPI_h):
     # using only Horizontal EPI, not 4D lightfield
-    # EPI_h: U*S*C
+    # EPI_h: V*T*C
 
-    [U,S,C] = EPI_h.shape
-    R_sd = np.zeros((U,C))
+    [V,T,C] = EPI_h.shape
+    R_sd = np.zeros((V,C))
 
-    u = 0
-    while s+(uHat - u)*d < S and u < U:   # make sure not over the boundary
+    v = 0
+    while t+(vHat - v)*d < T and v < V:   # make sure not over the boundary
         # save pixel intensity alone epipolar line
-        R_sd[u, :] = EPI_h[u, s + (uHat - u) * d, :]
-        u += 1
+        R_sd[v, :] = EPI_h[v, t + (vHat - v) * d, :]
+        v += 1
 
-    R_sd = R_sd[0:u-1,:]
+    R_sd = R_sd[0:v-1,:]
 
     return R_sd
 
 def K(x):
     # kernel
-    # x is a 3 channel EPI radiance
+    # x is a 3 channel EPI radiance, N*3
 
     h = 0.02
-    if np.linalg.norm(x/h) < 1:
-        return 1 - np.linalg.norm(x/h)**2
-    else:
-        return 0
 
-def r_bar_noiseFree(r_bar0,R_sd):
+    normVal = np.linalg.norm(x/h,axis=1)
+    mask = normVal < 1
+
+    return np.multiply(mask, 1 - np.square(normVal))    # 1*N
+
+    # if np.linalg.norm(x/h,axis=1) < 1:
+    #     return 1 - np.linalg.norm(x/h,axis=1)**2
+    # else:
+    #     return 0
+
+
+def r_bar_noiseFree(r_bar0,R_td):
     # for noisy EPI
     # r_bar: 1*3, EPI pixel intensity
-    # R_sd: N*C, N depend on the length of epipolar line in EPI, 0 < N < U
+    # R_td: N*C, N depend on the length of epipolar line in EPI, 0 < N < U
 
     iterNum = 10   # number of iteration
 
-    [N,C] = R_sd.shape
+    [N,C] = R_td.shape
     r_bar = r_bar0
     val1 = np.zeros_like(r_bar)
     val2 = np.zeros_like(r_bar)
+
     for iter in range(iterNum):
-        for n in range(N):
-            val1 = val1 + K(R_sd[n,:] - r_bar) * R_sd[n,:]
-            val2 = val2 + K(R_sd[n,:] - r_bar)
+        # for n in range(N):
+        #     val1 = val1 + K(R_sd[n,:] - r_bar) * R_sd[n,:]
+        #     val2 = val2 + K(R_sd[n,:] - r_bar)
+
+        # removed for loop
+        # K(R_sd[n,:] - r_bar): 1*N
+        # R_sd: N*3
+        temp = K(R_td - r_bar)
+        val1 = np.sum(np.multiply(R_td,K(R_td - r_bar)[:,np.newaxis]),axis=0)    #
+        val2 = np.sum(K(R_td - r_bar))
+
         r_bar = val1 / (val2 + 0.0000001)     # update, avoid NAN
 
-    return r_bar
+    return r_bar  # 1*3
 
+def refinedConfidence(vHat,Ce, depthScore):
+    # refined confidence at fixed uHat
+    # Ce: V*T
+    # depthScore: T*D
+
+    maxScore = np.amax(depthScore,axis=1)
+    avgScore = np.mean(depthScore,axis=1)
+    # at vHat
+    Cd_vHat = np.multiply(Ce[vHat,:], np.abs(maxScore - avgScore))  # 1*S
+
+    print(Cd_vHat.shape)
+
+    return Cd_vHat
+
+# def depthBilateralMedian(D_uHat,):
+#     # remove outliers in depth estimation, fix
+    # TODO: depth bilateral median filter
+
+
+def getDepthScore(vHat,T,D,EPI_h):
+    # using only horizontal EPI
+    # EPI_h = L(u0,:,s0,:,:), fixed u and s, V*T*C
+    depthScore = np.zeros((T, D))  # depthScore: T*D
+    for d in range(D):
+        for t in range(T):
+            R_td = getR_Horizontal(t, d, vHat, EPI_h)  # R: N*C, 0 < N < V
+            [N, _] = R_td.shape
+
+            r_bar = EPI_h[vHat, t, :]  # EPI_h: V*T*C
+            r_bar = r_bar_noiseFree(r_bar, R_td)  # r_bar: 1*3
+
+            # sumVal = 0
+            # for n in range(N):
+            #     sumVal = sumVal + K(R_sd[n,:] - r_bar)
+
+            # remove looping
+            sumVal = K(R_td - r_bar)
+            sumVal = np.sum(sumVal)
+
+            depthScore[t, d] = sumVal / N  # divided by the size of R, scalar
+
+        print(d, "/", D)
+
+    # plt.matshow(depthScore)
+    # plt.show()
+
+    return depthScore
--- src/main.py
+++ src/main.py
@@ -8,14 +8,15 @@ from utils import (
     loadMat,reconImg
 )
 from func import (
-    edgeConfidence,getR_Horizontal,K,r_bar_noiseFree
+    edgeConfidence,getR_Horizontal,K,r_bar_noiseFree,refinedConfidence,getDepthScore
 )
 
 if __name__ == "__main__":
 
     # light field image captured by Lytro ILLUM
     # demosaiced light field image, processed by MATLAB file
-    matPath = "../data/lightfield2.mat"
+    # matPath = "../data/lightfield2.mat"
+    matPath = "../data/guide.mat"
 
     # read 5D focal stack, processed by MATLAB
     L = loadMat(matPath)      # L(u,v,s,t,c)
@@ -30,19 +31,20 @@ if __name__ == "__main__":
     #######################################
     # extract EPI
 
-    # horizontal EPI, fix v and t, EPI: U*S*C
-    v = int(np.floor(V/2))  # scan vertical center line
-    t = int(np.floor(T/2))
-    EPI_h = L[:,v,:,t,:]
-    EPI_h = np.reshape(EPI_h,(U,S,C))
+    # vertical EPI, fix v and t, EPI: U*S*C
+    v0 = int(np.floor(V/2))  # scan vertical center line
+    t0 = int(np.floor(T/2))
+    EPI_v = L[:,v0,:,t0,:]
+    EPI_v = np.reshape(EPI_v,(U,S,C))
+    # io.imshow(EPI_v)
     # plt.show()
 
-    # vertical EPI, fix u and s, EPI: V*T*C
-    u = int(np.floor(U/2))  # scan horizontal center line
-    s = int(np.floor(S/2))
-    EPI_v = L[u,:,s,:,:]
-    EPI_v = np.reshape(EPI_v,(V,T,C))
-    # io.imshow(EPI_v)
+    # horizontal EPI, fix u and s, EPI: V*T*C
+    u0 = int(np.floor(U/2))  # scan horizontal center line
+    s0 = int(np.floor(S/2))
+    EPI_h = L[u0,:,s0,:,:]
+    EPI_h = np.reshape(EPI_h,(V,T,C))
+    # io.imshow(EPI_h)
     # plt.show()
 
     #######################################
@@ -73,29 +75,30 @@ if __name__ == "__main__":
 
     # range of disparity
     D = 20      # 0 to 20
+    print("Computing depth score")
 
+    # initialize
     uHat = int(np.floor(U/2))     # make uHat the horizontal centerline
     vHat = int(np.floor(V/2))     # make vHat the horizontal centerline
 
     # using only horizontal EPI for now
+    # EPI_h = L(u0,:,s0,:,:), fixed u and s, V*T*C
     # compute depth score
-    depthScore = np.zeros((S,D))  # depthScore: S*D*C
-    for d in range(D):
-        for s in range(S):
-            R_sd = getR_Horizontal(s, d, uHat, EPI_h)   # R: N*C, 0 < N < U
-            [N,_] = R_sd.shape
-
-            r_bar = EPI_h[uHat,s,:]     # EPI_h: U*S*C
-            r_bar = r_bar_noiseFree(r_bar,R_sd)     # r_bar: 1*3
-
-            sumVal = 0
-            for n in range(N):
-                sumVal = sumVal + K(R_sd[n,:] - r_bar)
-
-            depthScore[s,d] = sumVal / N              # divided by the size of R, scalar
-
-    print(depthScore.shape)
+    depthScore_vHat = getDepthScore(vHat,T,D,EPI_h)
 
     # pixel depth estimate
-    D_uHat = np.argmax(depthScore,axis=1)   # D(uHat,s): S*1
-    print(D_uHat)
+    D_vHat = np.argmax(depthScore_vHat,axis=1)   # D(vHat,t): T*1
+    # print(D_vHat)
+
+    # compute refined confidence, only on horizontal, fixed uHat, Cd(vHat, t)
+    Cd_vHat = refinedConfidence(vHat,Ce_h,depthScore_vHat)  # T*1
+    # print(Cd_vHat.shape)
+
+    # bilateral mediant filter on Depth estimate
+    # TODO: need to build this median filter
+
+
+    # Store confident depth estimation as 3D rays
+    Gamma = np.zeros((D,V,T,3))         # Gamma(d,vHat,t,r_bar.T): D,u,s,3
+    epsilon = 0.01                       # same as the epsilon used in bilateral filter, original epsilon = 0.1
+    processed = Cd_vHat > epsilon       # masks
